d54c9d60a9e2f6126461aa4fd8c80c71df4146b5
p000ison <maximilian.ammann@googlemail.com>
2015-01-23 21:34:34 +0100

    Add a few methods

--- src/main/java/org/societies/api/sieging/SiegeController.java
+++ src/main/java/org/societies/api/sieging/SiegeController.java
@@ -3,6 +3,8 @@ package org.societies.api.sieging;
 import org.societies.bridge.Location;
 import org.societies.groups.group.Group;
 
+import java.util.Set;
+
 /**
  * Represents a SiegeController
  */
@@ -11,6 +13,12 @@ public interface SiegeController {
     Siege start(Group group, City city, Location location);
 
     Siege start(Group group, City city, Location location, Wager wager);
+
+    Set<Siege> getSieges(City city);
+
+    Set<Siege> getSieges(Group group);
+
+    Set<Siege> getSiegesAgainst(Group group);
 
     void stop(Group group, City city);
 }
