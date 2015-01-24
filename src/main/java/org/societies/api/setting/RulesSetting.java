3923b5671b8d127e8d2b1314517d481599ae1d64
p000ison <maximilian.ammann@googlemail.com>
2015-01-24 12:31:28 +0100

    Some more refactoring

---
+++ src/main/java/org/societies/api/setting/RulesSetting.java
@@ -0,0 +1,29 @@
+package org.societies.api.setting;
+
+import org.societies.groups.setting.BooleanSetting;
+
+/**
+ * Represents a RulesSetting
+ */
+public class RulesSetting extends BooleanSetting {
+
+    public static final int ID = 0xFFF;
+
+    private final String rule;
+
+    public RulesSetting(String rule, int id) {
+        super(ID + id);
+        this.rule = rule;
+    }
+
+    public String getRule() {
+        return rule;
+    }
+
+    @Override
+    public String toString() {
+        return "RulesSetting{" +
+                "rule='" + rule + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/org/societies/api/setting/VerifySetting.java
@@ -0,0 +1,15 @@
+package org.societies.api.setting;
+
+import org.societies.groups.setting.BooleanSetting;
+
+/**
+ * Represents a VerifySetting
+ */
+public class VerifySetting extends BooleanSetting {
+
+    public static final int ID = 0x3;
+
+    public VerifySetting() {
+        super(ID);
+    }
+}
