166110fece93afe5a0c1b1b10c5b4194ba4ae92e
p000ison <maximilian.ammann@googlemail.com>
2015-01-31 22:35:38 +0100

    Updated sieging

--- src/main/java/org/societies/api/sieging/SiegeController.java
+++ src/main/java/org/societies/api/sieging/SiegeController.java
@@ -1,6 +1,5 @@
 package org.societies.api.sieging;
 
-import com.google.common.util.concurrent.ListenableFuture;
 import org.societies.bridge.Location;
 import org.societies.groups.group.Group;
 
@@ -12,17 +11,17 @@ import java.util.UUID;
  */
 public interface SiegeController {
 
-    ListenableFuture<Siege> start(Group group, City city, Location location);
+    Siege start(Group group, City city, Location location);
 
-    ListenableFuture<Siege> start(Group group, City city, Location location, Wager wager);
+    Siege start(Group group, City city, Location location, Wager wager);
 
-    ListenableFuture<Siege> getSiege(UUID uuid);
+    Siege getSiege(UUID uuid);
 
-    ListenableFuture<Set<Siege>> getSieges(City city);
+    Set<Siege> getSieges(City city);
 
-    ListenableFuture<Set<Siege>> getSieges(Group besieger);
+    Set<Siege> getSieges(Group besieger);
 
-    ListenableFuture<Set<Siege>> getSiegesAgainst(Group victim);
+    Set<Siege> getSiegesAgainst(Group victim);
 
     void stop(Siege siege);
 }
